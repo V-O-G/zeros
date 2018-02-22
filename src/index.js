e7bb8adefb89cfb1543a74cc7e5c46fcb04d02d9
V-O-G <cornflower1989@gmail.com>
2018-02-22 13:49:34 +0300

    zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  var summands = [];
+summands.push(Math.floor(number/10*2));
+var i=2;
+while (true){
+    var devisor = Math.pow(5,i);
+    summands.push(Math.floor(number/devisor));
+    i++;
+    if(number/devisor<1){break;}
+}
+var answer = summands.reduce(function(a, b) { return a + b; }, 0);
+return answer;
 }
